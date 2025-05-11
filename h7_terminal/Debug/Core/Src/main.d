49e2cd3dbefe3202d2e0cc2c1538f164f6ebfc24
Andrewww00 <andrea.longo333@gmail.com>
2025-05-11 12:47:08 +0200

    Initial commit

---
+++ h7_terminal/Debug/Core/Src/main.d
@@ -0,0 +1,106 @@
+Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal.h \
+ ../Core/Inc/stm32h7xx_hal_conf.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_rcc.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32H7xx/Include/stm32h7xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32H7xx/Include/stm32h723xx.h \
+ ../Drivers/CMSIS/Include/core_cm7.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32H7xx/Include/system_stm32h7xx.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_rcc_ex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_gpio.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_gpio_ex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_dma.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_dma_ex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_mdma.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_exti.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_cortex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_crc.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_crc_ex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_flash.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_flash_ex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_hsem.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_i2c.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_i2c_ex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_pwr.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_pwr_ex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_uart.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_uart_ex.h \
+ ../Drivers/BSP/STM32H7xx_Nucleo/stm32h7xx_nucleo.h \
+ ../Core/Inc/stm32h7xx_nucleo_conf.h \
+ ../Drivers/BSP/STM32H7xx_Nucleo/stm32h7xx_nucleo_errno.h \
+ ../MEMS/App/app_mems.h \
+ ../Middlewares/ST/STM32_MotionFX_Library/Inc/motion_fx.h \
+ ../Drivers/BSP/IKS4A1/iks4a1_motion_sensors.h \
+ ../MEMS/Target/iks4a1_conf.h ../Core/Inc/stm32h7xx_nucleo_bus.h \
+ ../Core/Inc/stm32h7xx_nucleo_conf.h ../Core/Inc/stm32h7xx_nucleo_errno.h \
+ ../Core/Inc/stm32h7xx_nucleo_errno.h \
+ ../Drivers/BSP/Components/Common/motion_sensor.h \
+ ../Drivers/BSP/Components/lis2mdl/lis2mdl.h \
+ ../Drivers/BSP/Components/lis2mdl/lis2mdl_reg.h \
+ ../Drivers/BSP/Components/lsm6dsv16x/lsm6dsv16x.h \
+ ../Drivers/BSP/Components/lsm6dsv16x/lsm6dsv16x_reg.h \
+ ../Drivers/BSP/Components/lis2duxs12/lis2duxs12.h \
+ ../Drivers/BSP/Components/lis2duxs12/lis2duxs12_reg.h \
+ ../Drivers/BSP/Components/lsm6dso16is/lsm6dso16is.h \
+ ../Drivers/BSP/Components/lsm6dso16is/lsm6dso16is_reg.h \
+ ../Drivers/BSP/Components/lsm6dso16is/lsm6dso16is_reg.h
+../Core/Inc/main.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal.h:
+../Core/Inc/stm32h7xx_hal_conf.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_rcc.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_def.h:
+../Drivers/CMSIS/Device/ST/STM32H7xx/Include/stm32h7xx.h:
+../Drivers/CMSIS/Device/ST/STM32H7xx/Include/stm32h723xx.h:
+../Drivers/CMSIS/Include/core_cm7.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32H7xx/Include/system_stm32h7xx.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_rcc_ex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_gpio.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_gpio_ex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_dma.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_dma_ex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_mdma.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_exti.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_cortex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_crc.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_crc_ex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_flash.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_flash_ex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_hsem.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_i2c.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_i2c_ex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_pwr.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_pwr_ex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_uart.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_uart_ex.h:
+../Drivers/BSP/STM32H7xx_Nucleo/stm32h7xx_nucleo.h:
+../Core/Inc/stm32h7xx_nucleo_conf.h:
+../Drivers/BSP/STM32H7xx_Nucleo/stm32h7xx_nucleo_errno.h:
+../MEMS/App/app_mems.h:
+../Middlewares/ST/STM32_MotionFX_Library/Inc/motion_fx.h:
+../Drivers/BSP/IKS4A1/iks4a1_motion_sensors.h:
+../MEMS/Target/iks4a1_conf.h:
+../Core/Inc/stm32h7xx_nucleo_bus.h:
+../Core/Inc/stm32h7xx_nucleo_conf.h:
+../Core/Inc/stm32h7xx_nucleo_errno.h:
+../Core/Inc/stm32h7xx_nucleo_errno.h:
+../Drivers/BSP/Components/Common/motion_sensor.h:
+../Drivers/BSP/Components/lis2mdl/lis2mdl.h:
+../Drivers/BSP/Components/lis2mdl/lis2mdl_reg.h:
+../Drivers/BSP/Components/lsm6dsv16x/lsm6dsv16x.h:
+../Drivers/BSP/Components/lsm6dsv16x/lsm6dsv16x_reg.h:
+../Drivers/BSP/Components/lis2duxs12/lis2duxs12.h:
+../Drivers/BSP/Components/lis2duxs12/lis2duxs12_reg.h:
+../Drivers/BSP/Components/lsm6dso16is/lsm6dso16is.h:
+../Drivers/BSP/Components/lsm6dso16is/lsm6dso16is_reg.h:
+../Drivers/BSP/Components/lsm6dso16is/lsm6dso16is_reg.h:
---
+++ h7_terminal/Debug/MEMS/App/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../MEMS/App/app_mems.c 
+
+OBJS += \
+./MEMS/App/app_mems.o 
+
+C_DEPS += \
+./MEMS/App/app_mems.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+MEMS/App/%.o MEMS/App/%.su MEMS/App/%.cyclo: ../MEMS/App/%.c MEMS/App/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DBUTTON_KEY=BUTTON_USER -DUSE_NUCLEO_144 -DUSE_NUCLEO_H723ZG -DUSE_PWR_LDO_SUPPLY -DUSE_NUCLEO_64 -DUSE_HAL_DRIVER -DSTM32H723xx -c -I../Core/Inc -I../Drivers/STM32H7xx_HAL_Driver/Inc -I../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy -I../Drivers/BSP/STM32H7xx_Nucleo -I../Drivers/CMSIS/Device/ST/STM32H7xx/Include -I../Drivers/CMSIS/Include -I../MEMS/App -I../MEMS/Target -I../Drivers/BSP/Components/lsm6dsv16x -I../Drivers/BSP/Components/lis2duxs12 -I../Drivers/BSP/Components/lis2mdl -I../Drivers/BSP/Components/lsm6dso16is -I../Drivers/BSP/Components/sht40ad1b -I../Drivers/BSP/Components/lps22df -I../Drivers/BSP/Components/stts22h -I../Drivers/BSP/IKS4A1 -I../Drivers/BSP/Components/Common -I../Middlewares/ST/STM32_MotionFX_Library/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-MEMS-2f-App
+
+clean-MEMS-2f-App:
+	-$(RM) ./MEMS/App/app_mems.cyclo ./MEMS/App/app_mems.d ./MEMS/App/app_mems.o ./MEMS/App/app_mems.su
+
+.PHONY: clean-MEMS-2f-App
+
